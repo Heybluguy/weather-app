32fd0226ea227fbb92015eb749bb024b5c65f663
Luis  Senior <heybluguy@yahoo.com>
2019-10-08 16:43:03 -0400

    key

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import Titles from './components/Titles';
 import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = '5fffdb579b7c80f227f0fcaee058413d';
 
 class App extends React.Component {
 	render() {
@@ -9,6 +12,7 @@ class App extends React.Component {
 			<div>
 				<Titles />
 				<Form />
+				<Weather />
 			</div>
 		);
 	}
